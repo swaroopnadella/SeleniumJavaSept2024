52e2f78d48d89bd85d64f79c926e48b6753575f0
Swaroop Nadella <swaroopnadella@hotmail.com>
2024-12-08 18:58:35 +0530

    remove Set import

--- seleniumjavapractice/src/test/java/day20/KeyboardActionsNewTabDemo.java
+++ seleniumjavapractice/src/test/java/day20/KeyboardActionsNewTabDemo.java
@@ -3,7 +3,6 @@ package day20;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
